3eaa34f7abe14f744664f30ea87086ccd75e6dda
for2084 <for2084@users.noreply.github.com>
2019-05-16 20:12:22 +0800

    Update mysqlidb.php

--- inc/php/mysqlidb.php
+++ inc/php/mysqlidb.php
@@ -1,7 +1,7 @@
 <?php
   class mysqlidb extends mysqli {
     public function __construct($db){
-      parent::__construct("localhost","root","lfw",$db);
+      parent::__construct("localhost","root","xxx",$db);
       if(mysqli_connect_error()){
         die('Connect Error (' .mysqli_connect_errno() .')' .mysqli_connect_error());
         exit(1);
